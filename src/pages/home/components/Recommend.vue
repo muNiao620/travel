72d19c70d146363c7162ee323716633eabeaeb8a
muNiao620 <a@adeMacBook-Pro.local>
2019-08-27 23:52:56 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key='item.id'>
+				<img class="item-img" :src="item.imgUrl" />
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeRecommend',
+	data () {
+		return {
+			recommendList: [{
+				id: '0001',
+				imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg',
+				title: '北京欢乐谷',
+				desc: '亚洲唯一飞行式过山车等你来挑战'
+				},{
+				id: '0002',
+				imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
+				title: '奥林匹克塔',
+				desc: ''
+				},{
+				id: '0003',
+				imgUrl: 'http://img1.qunarzz.com/sight/p0/201308/08/41150dacc0e3a0c8c8d65eac.jpg_200x200_c62292e9.jpg',
+				title: '北京动物园',
+				desc: '百余种动物，让你离自然更近'
+			}]
+		} 
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/mixins.styl'
+	.title
+		margin-top: .2rem
+		line-height: .8rem
+		background: #eee
+		text-indent: .2rem
+	.item
+		overflow: hidden
+		display: flex
+		height: 1.9rem
+		.item-img
+			width: 1.7rem
+			height: 1.7rem
+			padding: .1rem
+		.item-info
+			flex: 1
+			padding: .1rem
+			min-width: 0
+			.item-title
+				line-height: .54rem
+				font-size: .32rem
+				ellipsis()
+			.item-desc
+				line-height: .4rem
+				color: #ccc
+				ellipsis()
+			.item-button
+				line-height: .44rem
+				margin-top: .16rem
+				background: #ff9300
+				padding: 0 .1rem
+				border-radius: .06rem
+				color: #fff
+</style>
